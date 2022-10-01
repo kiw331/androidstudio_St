e1b0ed6fe14392e8b6059f35dce3ab951119f7bb
kiwi_j500 <mingureion@gmail.com>
2022-10-01 23:15:41 +0900

    aa

--- unimarket-app/Unimarket_dv/app/src/main/java/com/example/unimarket_dv/MainActivity.java
+++ unimarket-app/Unimarket_dv/app/src/main/java/com/example/unimarket_dv/MainActivity.java
@@ -1,15 +1,15 @@
-package org.techtown.tab;
-
-import android.os.Bundle;
-import android.view.MenuItem;
-import android.widget.Toast;
+package com.example.unimarket_dv;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Bundle;
+import android.view.MenuItem;
+
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
     Fragment1 fragment1;
     Fragment2 fragment2;
     Fragment3 fragment3;
@@ -19,35 +19,39 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         //프래그먼트생성
         fragment1 = new Fragment1();
         fragment2 = new Fragment2();
         fragment3 = new Fragment3();
 
         //처음엔 검색탭에서 시작
-        getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment1).commit();
-        BottomNavigationView bottomNavigation = findViewById(R.id.bottom_navigation);
-        
+        getSupportFragmentManager().beginTransaction().replace(R.id.main_frame, fragment1).commit();
+        BottomNavigationView bottomNavigation = findViewById(R.id.bottomNavi);
+
         // 하단 네비게이터 터치시 fragment 변경
-        bottomNavigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+        bottomNavigation.setOnItemSelectedListener(new BottomNavigationView.OnItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 switch (item.getItemId()) {
-                    case R.id.tab1:
+                    case R.id.search1:
                         getSupportFragmentManager().beginTransaction()
-                                .replace(R.id.container, fragment1).commit();
+                                .replace(R.id.main_frame, fragment1).commit();
                         return true;
-                    case R.id.tab2:
+                    case R.id.notice2:
                         getSupportFragmentManager().beginTransaction()
-                                .replace(R.id.container, fragment2).commit();
+                                .replace(R.id.main_frame, fragment2).commit();
                         return true;
-                    case R.id.tab3:
+                    case R.id.setting3:
                         getSupportFragmentManager().beginTransaction()
-                                .replace(R.id.container, fragment3).commit();
+                                .replace(R.id.main_frame, fragment3).commit();
                         return true;
                 }
                 return false;
             }
         });
     }
+
+
+
 }
