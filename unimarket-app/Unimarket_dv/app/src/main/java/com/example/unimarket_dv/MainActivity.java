0398fbefa3a418c6591e1dea8fb1c9c33bbb7fb8
kiwi_j500 <mingureion@gmail.com>
2022-12-02 20:19:42 +0900

    D: 12.2.1

--- unimarket-app/Unimarket_dv/app/src/main/java/com/example/unimarket_dv/MainActivity.java
+++ unimarket-app/Unimarket_dv/app/src/main/java/com/example/unimarket_dv/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.Toast;
+
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
